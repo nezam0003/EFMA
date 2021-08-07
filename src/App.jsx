76403593eee73224bd6e272a018debcfdbf472d8
nezam0003 <fhameedk.nezam@gmail.com>
2021-08-07 16:20:12 +0600

    Title done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Title from "./components/Title";
 import TopHeader from "./components/TopHeader";
 
 const App = () => {
   return (
     <>
       <TopHeader />
+      <Title />
     </>
   );
 };
---
+++ src/components/Title.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <>
+      <section className="py-5">
+        <div className="container border-bottom border-1 border-danger pb-3">
+          <div className="row">
+            <div className="col-11 mx-auto text-danger text-capitalize  text-center">
+              <h5 className="">European Financial Management Association</h5>
+              <h5>2021 Annual Meeting</h5>
+              <h5>June 30 - July 03, 2021</h5>
+              <h5>Hosted by the University of Leeds</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Title;
